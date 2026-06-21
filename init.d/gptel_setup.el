f19f1209fdbe25031f14c202f2c4dd747dd07a94
Ignacio Randazzo <ignacio@randazzo.ar>
2026-06-21 04:04:22 -0300

    Up the num_predict for all models

--- init.d/gptel_setup.el
+++ init.d/gptel_setup.el
@@ -7,6 +7,12 @@
                                                :host "192.168.2.69:11434"
                                                :stream t
                                                :models '("granite4.1:8b-q8_0" "gpt-oss:20b" "gpt-oss:120b" "mistral-medium-3.5:128b" "nemotron-3-super:120b")
-                                               :request-params '(:options (:temperature 0.7 :top_p 0.95 :num_ctx 1048576))))
+                                               :request-params '(:options (
+									:temperature 0.7 
+							 		:top_p 0.95 
+									:num_ctx 1048576
+									:num_predict 1048576
+								))))
+
 
 (setq-default gptel-model 'nemotron-3-super:120b)
